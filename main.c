78c61ed5afd440dfd29c74232e1602e70b987a43
Sebastian Krzyszkowiak <dos@dosowisko.net>
2013-01-27 21:13:05 +0100

    prototype of client ui

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int main(int argc, char** argv) {
-  printf("Derp.\n");
+
+void on_window_destroy (GObject* object, gpointer user_data) {
+		gtk_main_quit ();
+}
+
+int main (int argc, char** argv) {
+	gtk_init(&argc, &argv);
+
+	GtkBuilder *builder;
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder, "../interface.xml", NULL); // FIXME
+		GtkWidget *window;
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+	gtk_builder_connect_signals(builder, NULL);
+
+	g_object_unref(G_OBJECT(builder));
+
+	gtk_widget_show(window);
+	gtk_main();
   return 0;
 }
